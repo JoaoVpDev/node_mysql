944a33c43877dcd5c832e32f27c5a056eab3126f
joaovpdev <joaovictorpereira1@live.com>
2024-07-23 09:45:57 -0300

    código 9_preparar_query comentado

--- 9_preparar_query/db/conn.js
+++ 9_preparar_query/db/conn.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql')
+const mysql = require('mysql') // Importa o módulo MySQL
 
 
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql2',
+const pool = mysql.createPool({ // Cria um pool de conexões com o banco de dados
+    connectionLimit: 10, // Define o limite máximo de conexões no pool
+    host: 'localhost', // Endereço do servidor do banco de dados
+    user: 'root', // Nome de usuário para se conectar ao banco de dados
+    password: '', // Senha do usuário para se conectar ao banco de dados
+    database: 'nodemysql2', // Nome do banco de dados ao qual se conectar
 })
 
-module.exports = pool
+module.exports = pool // Exporta o pool de conexões para ser usado em outros arquivos
--- 9_preparar_query/index.js
+++ 9_preparar_query/index.js
@@ -1,133 +1,141 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const pool = require('./db/conn')
+const express = require("express") //impora o módulo express
+const exphbs = require("express-handlebars") //importa o módulo express-handlebars para teamplates
+const pool = require('./db/conn') //importa a conexão com o banco de dados
 
-const app = express()
+const app = express() // cria uma instância do express
 
-app.engine("handlebars", exphbs.engine())
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs.engine()) //configura o motor de teamplates Handlebars
+app.set('view engine', 'handlebars') //define Handlebars como o motor de visualização
 
 app.use(
     express.urlencoded({
-        extended: true
+        extended: true //permite codificação de URL estendida
     })
 )
 
-app.use(express.json())
+app.use(express.json()) //permite receber dados JSON nas requisições
 
-app.use(express.static('public'))
+app.use(express.static('public')) //define o diretório 'public' para servir arquivos estáticos
 
-
+//Rota da página inicial
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home') //renderiza o teamplate home
 })
 
+//Rota para inserir um novo livro
 app.post('/books/insertbook', (req, res) => {
-    const title = req.body.title
-    const pageqty = req.body.pageqty
+    const title = req.body.title //Obtém o título do livro
+    const pageqty = req.body.pageqty //Obtém a quantidade de páginas do livro do corpo da requisição
 
-    const sql = `INSERT INTO books (??, ??) VALUES (?, ?)`
-    const data = ['title', 'pageqty', title, pageqty]
+    const sql = `INSERT INTO books (??, ??) VALUES (?, ?)` //SQL para inserir um novo livro no banco
+    const data = ['title', 'pageqty', title, pageqty] //dados da consula
 
 
-    pool.query(sql, data, function (err) {
+    pool.query(sql, data, function (err) { //exeuta a consulta SQL
         if (err) {
-            console.log(err)
+            console.log(err) //Impreme um erro caso ocorra
         }
-        res.redirect('/')
+        res.redirect('/') //redireciona para a página inicial
     })
 })
 
+//Rota para listar todos os livros
 app.get('/books', (req, res) => {
-    const sql = 'SELECT * FROM books'
+    const sql = 'SELECT * FROM books' //SQL para selecionar todos os livros da table
 
-    pool.query(sql, function (err, data) {
+    pool.query(sql, function (err, data) { // executa consulta SQL
         if (err) {
-            console.log(err)
+            console.log(err) //Impreme um erro caso ocorra
             return
         }
 
-        const books = data
+        const books = data //armazena os dados dos livros
 
 
-        res.render('books', { books })
+        res.render('books', { books }) // Renderiza o template 'books' com os dados dos livros
     })
 })
 
+//Rota para exibir detalhes de um livro específico
 app.get('/books/:id', (req, res) => {
-    const id = req.params.id
+    const id = req.params.id //Obtém o ID do livro dos parâmetros da requisição
 
-    const sql = `SELECT * FROM books WHERE ?? = ?`
-    const data = ['id', id]
+    const sql = `SELECT * FROM books WHERE ?? = ?` // SQL para selecionar um livro pelo ID
+    const data = ['id', id] //dados da consulta
 
-    pool.query(sql, data, function (err, data) {
+    pool.query(sql, data, function (err, data) { //executa a consulta SQL
         if (err) {
-            console.log(err)
+            console.log(err)//Impreme um erro caso ocorra
             return
         }
 
-        const book = data[0]
+        const book = data[0] //armazena dados do livro
 
-        res.render('book', { book })
+        res.render('book', { book }) //Renderiza o teamplate book coms os dados do livro
     })
 
 })
 
+
+// Rota para editar um livros específico
 app.get('/books/edit/:id', (req, res) => {
 
-    const id = req.params.id
+    const id = req.params.id  // Obtém o ID do livro dos parâmetros da requisição
 
-    const sql = `SELECT * FROM books WHERE ?? = ?`
-    const data = ['id', id]
+    const sql = `SELECT * FROM books WHERE ?? = ?` // SQL para selecionar um livro pelo ID
+    const data = ['id', id] // Dados para a consulta
 
-    pool.query(sql, data, function (err, data) {
+    pool.query(sql, data, function (err, data) { // Executa a consulta SQL
         if (err) {
-            console.log(err)
+            console.log(err) //Impreme um erro caso ocorra
             return
         }
 
-        const book = data[0]
+        const book = data[0] // Armazena os dados do livro
 
-        res.render('editbook', { book })
+        res.render('editbook', { book }) // Renderiza o template 'editbook' com os dados do livro
     })
 })
 
+
+// Rota para atualizar um livro
 app.post('/books/updatebook', (req, res) => {
 
-    const id = req.body.id
-    const title = req.body.title
-    const pageqty = req.body.pageqty
+    const id = req.body.id // Obtém o ID do livro do corpo da requisição
+    const title = req.body.title // Obtém o título do livro do corpo da requisição
+    const pageqty = req.body.pageqty // Obtém a quantidade de páginas do livro do corpo da requisição
 
-    const sql = `UPDATE books SET ?? = ?, ?? = ? WHERE ?? = ?`
+    const sql = `UPDATE books SET ?? = ?, ?? = ? WHERE ?? = ?` // SQL para atualizar um livro
 
-    const data = ['title', title, 'pageqty', pageqty, 'id', id]
+    const data = ['title', title, 'pageqty', pageqty, 'id', id] // Dados para a consulta
 
-    pool.query(sql, data, function (err) {
+    pool.query(sql, data, function (err) { // Executa a consulta SQL
         if (err) {
-            console.log(err)
+            console.log(err) //Impreme um erro caso ocorra
             return
         }
 
-        res.redirect(`/books`)
+        res.redirect(`/books`) // Redireciona para a lista de livros após editado
     })
 })
 
 
+// Rota para remover um livro
 app.post('/books/remove/:id', (req, res) => {
 
-    const id = req.params.id
+    const id = req.params.id // Obtém o ID do livro dos parâmetros da requisição
 
-    const sql = `DELETE FROM books WHERE ?? = ?`
-    const data = ['id', id]
+    const sql = `DELETE FROM books WHERE ?? = ?` // SQL para deletar um livro
+    const data = ['id', id] // Dados para a consulta
 
-    pool.query(sql, data, function (err) {
+    pool.query(sql, data, function (err) { // Executa a consulta SQL
         if (err) {
-            console.log(err)
+            console.log(err) // Loga o erro, se houver
             return
         }
 
-        res.redirect('/books')
+        res.redirect('/books') // Redireciona para a lista de livros
     })
 })
 
-app.listen(3000)
+app.listen(3000) // Inicia o servidor na porta 3000
